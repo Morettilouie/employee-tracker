61b9c4e1d6bb1dc9b4a69241865b99db41acd976
Louie Moretti <morettilouie@gmail.com>
2022-08-23 23:22:34 -0500

    initial files and database added, trouble with the seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department(name)
+VALUES
+    ('Engineering'),
+    ('Finance'),
+    ('Legal'),
+    ('Sales');
+
+INSERT INTO role (title, department_id, salary)
+VALUES
+    ('Sales Lead', 4, 100000.00),
+    ('Salesperson', 4, 80000.00),
+    ('Lead Engineer', 1, 150000.00),
+    ('Software Engineer', 1, 120000.00),
+    ('Account Manager', 2, 160000.00),
+    ('Accountant', 2, 125000.00),
+    ('Legal Team Lead', 3, 250000.00),
+    ('Lawyer', 3, 190000.00);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, 0),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, 0),
+    ('Kevin', 'Tupik', 4, 2),
+    ('Kunal', 'Singh', 5, 0),
+    ('Malia', 'Brown', 6, 3),
+    ('Sarah', 'Lourd', 7, 0),
+    ('Tom', 'Allen', 8, 4);
