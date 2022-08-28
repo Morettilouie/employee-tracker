e893af9372e7bb0b7eee6e2d5caa5c4172f2e48b
Louie Moretti <morettilouie@gmail.com>
2022-08-27 20:44:39 -0500

    add features added

--- app.js
+++ app.js
@@ -6,7 +6,46 @@ var display = function (error, results) {
     console.table(results)
 }
 
-var startProgram = function () {
+const addEmployee = [
+    {
+        type: 'input',
+        name: 'firstName',
+        message: 'First name:'
+    },
+    {
+        type: 'input',
+        name: 'lastName',
+        message: 'Last name:'
+    },
+    // {
+    //     type: 'list',
+    //     name: 'role',
+    //     message: 'Role:',
+    //     choices: [
+
+    //     ]
+    // }
+]
+
+const addRole = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'New role:'
+    },
+    {
+        type: 'number',
+        name: 'department',
+        message: 'department:'
+    },
+    {
+        type: 'number',
+        name: 'salary',
+        message: 'salary:'
+    }
+]
+
+const startProgram = function () {
     inquirer
         .prompt({
             type: 'list',
@@ -31,16 +70,53 @@ var startProgram = function () {
                     LEFT JOIN role ON employee.role_id = role.id 
                     LEFT JOIN department ON role.department_id = department.id;`, display
                 )
+            } else if (data.option === 'Add Employee') {
+                inquirer
+                    .prompt(addEmployee)
+                    .then(employee => {
+                        db.query(
+                            `INSERT INTO employee (first_name, last_name)
+                            VALUES
+                            ('${employee.firstName}', '${employee.lastName}');`, display
+                        )
+                    })
+
             } else if (data.option === 'View All Roles') {
                 db.query(
                     `SELECT role.id, title, department.name as department, salary 
                     FROM role 
                     LEFT JOIN department ON department_id = department.id;`, display
                 )
+            } else if (data.option === 'Add Role') {
+                inquirer
+                    .prompt(addRole)
+                    .then(newRole => {
+                        db.query(
+                            `INSERT INTO role (title, department_id, salary)
+                            VALUES
+                            ('${newRole.title}', ${newRole.department}, ${newRole.salary});`, display
+                        )
+                    })
+
             } else if (data.option === 'View All Departments') {
                 db.query(
                     `SELECT * FROM department;`, display
                 )
+            } else if (data.option === 'Add Department') {
+                inquirer
+                    .prompt({
+                        type: 'input',
+                        name: 'department',
+                        message: 'New department:'
+                    })
+                    .then(newDepartment => {
+                        db.query(
+                            `INSERT INTO department (name)
+                            VALUES
+                            ('${newDepartment.department}');`, display
+                        )
+                    })
+
             }
         })
 }
