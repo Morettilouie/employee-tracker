26c151f60459a2fb1e1a4cd3af5ece6be75d011c
Louie Moretti <morettilouie@gmail.com>
2022-09-20 21:39:52 -0500

    0s changed to null in seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -18,11 +18,11 @@ VALUES
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ('John', 'Doe', 1, 0),
+    ('John', 'Doe', 1, null),
     ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, 0),
+    ('Ashley', 'Rodriguez', 3, null),
     ('Kevin', 'Tupik', 4, 3),
-    ('Kunal', 'Singh', 5, 0),
+    ('Kunal', 'Singh', 5, null),
     ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, 0),
+    ('Sarah', 'Lourd', 7, null),
     ('Tom', 'Allen', 8, 7);
